964ede426fca008c94a5e5d3fa151caef6d8cb0c
AAAAAAAArne <56074086+AAAAAAAArne@users.noreply.github.com>
2019-10-14 14:30:09 +0200

    uniformcostsearch_afgewerkt

--- search.py
+++ search.py
@@ -169,24 +169,30 @@ def backtrace(parent, end, directions):     # Find directions of shortest path
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    node = problem.getStartState()
-    visited = util.PriorityQueue()
-    visited.push((node, []), 0)
-    closed = set()  # geen index bij een set() en elk item komt maar 1 keer voor
-    co = {}
-    co[node] = 0
-    while True:
-        if visited.isEmpty():
-            return []
-        node, act = visited.pop()
+    # node is een state of eerder een positie
+    # actions gaat bijhouden welke directions moeten uitgegaan worden
+    # cost is hoeveel het kost om bij de beschouwde node te geraken
+    # priority zorgt er in de priorityqueue voor dat degene met de laagste priority als eerste wordt gepopt
+    
+    actions = []
+    node = (problem.getStartState(), actions,0) # node bevat state, actions (dus welke stappen ondernomen worden), en cost om er te geraken)
+                                                # eerst is de cost 0 want ge begint van deze positie
+    pQ = util.PriorityQueue()                   # maak priorityqueue
+    pQ.push(node, 0)                            # zet eerste node in de priorityqueue
+    visited = []                                # maak lege list om de bezochte node te bepalen
+
+    while pQ.isEmpty() != True:                 # check de pQ totdat die leeg is
+        (node, actions, stepCost) = pQ.pop()             # eigenschap van pQ is dat het item met de laagste cost wordt gepopt
         if problem.isGoalState(node):
-            return act
-        if node not in closed:
-            closed.add(node)
-            for child, action, cost in problem.getSuccessors(node):
-                upcost = co[node] + cost
-                co[child] = upcost
-                visited.push((child, act + [action]), upcost)
+            break
+        if node not in visited:
+            visited.append(node)
+            for successor in problem.getSuccessors(node):   # getSuccessor geeft (node, action, stepCost) terug als list
+                newAction = actions + [successor[1]]        # Kan alleen een list met list concatinaten daarom moet de string (successor[1]) nog eens tussen []
+                newStepCost = stepCost + successor[2]
+                newNode = (successor[0], newAction, newStepCost)
+                pQ.update(newNode, newStepCost)
+    return actions
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
