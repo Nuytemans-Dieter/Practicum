ce62d7f12a02c62719b63717b411707136ba56d5
Dieter Nuytemans <37397560+Nuytemans-Dieter@users.noreply.github.com>
2019-11-14 17:50:13 +0100

    Better print statements

--- Lab 4 - Chess Agent/train_stockfish.py
+++ Lab 4 - Chess Agent/train_stockfish.py
@@ -17,9 +17,9 @@ def main():
         move = None
 
         if turn_white_player:
-            value, move = white_player.minimax(board, 3, turn_white_player)
-            print("The best move gives value", value)
+            value, move = white_player.minimax(board, 1, turn_white_player)
             print("The current board value is", evaluate(board))
+            print("The best move gives value", value)
             turn_white_player = False
 
         else:
@@ -27,6 +27,7 @@ def main():
             turn_white_player = True
 
         board.push(move)
+        print("The value after moving is", evaluate(board))
         print(board)
         print("###########################")
 
