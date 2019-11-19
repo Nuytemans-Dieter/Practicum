41c038e149d667fcea2143ea96118a4827cf7a1a
Michiel Proost <michiel.proost@gmail.com>
2019-11-19 16:24:56 +0100

    Random / Random-with-first-level-search moved

--- Lab 4 - Chess Agent/searchagent/search_agent.py
+++ Lab 4 - Chess Agent/searchagent/search_agent.py
@@ -11,40 +11,6 @@ class SearchAgent(object):
         self.name = "Chess Engine"
         self.author = "S. Vanneste"
 
-    def random_move(self, board: chess.Board):
-        return random.sample(list(board.legal_moves), 1)[0]
-
-    def random_with_first_level_search(self, board: chess.Board):
-        moves = list(board.legal_moves)
-
-        best_move = random.sample(moves, 1)[0]
-        best_move_value = 0
-
-        for move in moves:
-            board.push(move)
-            if board.is_checkmate():
-                move_value = 100
-                if move_value > best_move_value:
-                    best_move = move
-            board.pop()
-
-            if board.is_into_check(move):
-                move_value = 90
-                if move_value > best_move_value:
-                    best_move = move
-
-            if board.is_capture(move):
-                move_value = 80
-                if move_value > best_move_value:
-                    best_move = move
-
-            if board.is_castling(move):
-                move_value = 70
-                if move_value > best_move_value:
-                    best_move = move
-
-        return best_move
-
     def minimax(self, board: chess.Board, depth, maximizingPlayer):
         if depth == 0 or board.is_checkmate():
             return evaluate(board), None
@@ -133,3 +99,37 @@ class SearchAgent(object):
                     alpha = score   # Capture that improves our last best move.
 
         return alpha
+
+    def random_move(self, board: chess.Board):
+        return random.sample(list(board.legal_moves), 1)[0]
+
+    def random_with_first_level_search(self, board: chess.Board):
+        moves = list(board.legal_moves)
+
+        best_move = random.sample(moves, 1)[0]
+        best_move_value = 0
+
+        for move in moves:
+            board.push(move)
+            if board.is_checkmate():
+                move_value = 100
+                if move_value > best_move_value:
+                    best_move = move
+            board.pop()
+
+            if board.is_into_check(move):
+                move_value = 90
+                if move_value > best_move_value:
+                    best_move = move
+
+            if board.is_capture(move):
+                move_value = 80
+                if move_value > best_move_value:
+                    best_move = move
+
+            if board.is_castling(move):
+                move_value = 70
+                if move_value > best_move_value:
+                    best_move = move
+
+        return best_move
