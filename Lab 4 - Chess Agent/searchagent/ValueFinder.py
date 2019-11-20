cc9b57b0d53b4cace0dc1793fc96bcf24c1eda6f
Dieter Nuytemans <37397560+Nuytemans-Dieter@users.noreply.github.com>
2019-11-20 15:17:37 +0100

    Small improvements + position preparations

--- Lab 4 - Chess Agent/searchagent/ValueFinder.py
+++ Lab 4 - Chess Agent/searchagent/ValueFinder.py
@@ -1,5 +1,5 @@
 import chess
-from time import sleep
+import time
 
 # Based on Hans Berliner's system
 # See: https://en.wikipedia.org/wiki/Chess_piece_relative_value
@@ -12,6 +12,76 @@ pieceValues = {
     'p': 10,    # Pawn   /  Pion
 }
 
+pawnPosValue = [
+     0,  0,  0,  0,  0,  0,  0,  0,
+     5, 10, 10,-20,-20, 10, 10,  5,
+     5, -5,-10,  0,  0,-10, -5,  5,
+     0,  0,  0, 20, 20,  0,  0,  0,
+     5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+     0,  0,  0,  0,  0,  0,  0,  0]
+
+knightPosValue = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50]
+
+bishopPosValue = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20]
+
+rookPosValue = [
+      0,  0,  0,  5,  5,  0,  0,  0,
+     -5,  0,  0,  0,  0,  0,  0, -5,
+     -5,  0,  0,  0,  0,  0,  0, -5,
+     -5,  0,  0,  0,  0,  0,  0, -5,
+     -5,  0,  0,  0,  0,  0,  0, -5,
+     -5,  0,  0,  0,  0,  0,  0, -5,
+      5, 10, 10, 10, 10, 10, 10,  5,
+     0,  0,  0,  0,  0,  0,  0,  0]
+
+queenPosValue = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+      0,  0,  5,  5,  5,  5,  0, -5,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20]
+
+kingPosValue = [
+     20, 30, 10,  0,  0, 10, 30, 20,
+     20, 20,  0,  0,  0,  0, 20, 20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30]
+
+posValue = [
+     1, 1, 1, 1, 1, 1, 1, 1,
+     1, 1, 1, 1, 1, 1, 1, 1,
+     2, 2, 2, 2, 2, 2, 2, 2,
+     2, 2, 4, 5, 5, 4, 2, 2,
+     2, 2, 4, 5, 5, 4, 2, 2,
+     2, 2, 2, 2, 2, 2, 2, 2,
+     1, 1, 1, 1, 1, 1, 1, 1,
+     1, 1, 1, 1, 1, 1, 1, 1]
+
 # Openings in FEN notation
 # Starting position: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 opening = chess.Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1')
@@ -20,32 +90,27 @@ opening = chess.Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 
 def evaluate(board):
 
     totalValue = 0
+    # print("---")
+    # print(board)
+    # print("---")
+    # sleep(0.1)  # Time in seconds
 
-    for i in range(0, 64):
-        piece = board.piece_at(i)
+
+    for position in range(0,64):            # Loop through the board
+        piece = board.piece_at( position )  # Get a piece
         if piece != None:
-            isWhite = bool( piece.color )
-            value = getPieceValue( str(piece) )
-            #print("---")
-            #print(board)
-            #print("---")
-            #sleep(0.2)  # Time in seconds
-            #if opening == board:
-            #    return 50000
-            if isWhite:
+
+            isWhite = bool( piece.color )   # Get the piece owner
+
+            value = getPieceValue( str(piece) )         # Convert the piece to a value
+            numAttacks = len(board.attacks( position )) # get the number of attacks this piece can do
+
+            if isWhite:                    # White pieces are considered positive
                 totalValue += value
-                attacks = board.attacks(i)
-
-                #print('---')
-                #print("For position: ", i)
-                #print("Row: ", (i-i%8)/8, " - Column:", i%8)
-                #print(board)
-                #print('')
-                #print(attacks)
-                #print("---")
-
-            else:
+                totalValue += numAttacks
+            else:                          # Black pieces are considered negative
                 totalValue -= value
+                totalValue -= numAttacks
 
     return totalValue
 
